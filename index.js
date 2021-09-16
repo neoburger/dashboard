798b0b61a6fb0f241361ee3d0f53bd641b465cf6
vang1ong7ang <vang1ong7ang@outlook.com>
2021-09-15 23:38:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+new Promise(resolve => resolve('0x48c40d4666f93408be1bef038b6722404d9a4c2a')).then(v => {
+  const value = document.createElement('code');
+  value.innerText = v;
+  const title = document.getElementById('bneo-script-hash');
+  title.parentElement.insertBefore(value, title.nextSibling)
+})
+
+new Promise(resolve => resolve('NPmdLGJN47EddqYcxixdGMhtkr7Z5w4Aos')).then(v => {
+  const value = document.createElement('code');
+  value.innerText = v;
+  const title = document.getElementById('bneo-script-hash');
+  title.parentElement.insertBefore(value, title.nextSibling)
+})
