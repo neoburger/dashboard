4528fc4f0fdf41babaa6719429755c8e212251ca
vang1ong7ang <vang1ong7ang@outlook.com>
2021-09-15 23:45:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ new Promise(resolve => resolve('0x48c40d4666f93408be1bef038b6722404d9a4c2a')).th
 new Promise(resolve => resolve('NPmdLGJN47EddqYcxixdGMhtkr7Z5w4Aos')).then(v => {
   const value = document.createElement('code');
   value.innerText = v;
-  const title = document.getElementById('bneo-script-hash');
+  const title = document.getElementById('bneo-contract-address');
   title.parentElement.insertBefore(value, title.nextSibling)
 })
