4fa00e94415608be9fbc49df9d15a60c7e428fb9
vang1ong7ang <vang1ong7ang@outlook.com>
2021-09-17 08:00:58 -0400

    cors test

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ new Promise(resolve => resolve('NPmdLGJN47EddqYcxixdGMhtkr7Z5w4Aos')).then(v => 
   const title = document.getElementById('bneo-contract-address');
   title.parentElement.insertBefore(value, title.nextSibling)
 })
+const ENDPOINT = 'https://neofura.ngd.network:1927';
+const BNEO = '0x48c40d4666f93408be1bef038b6722404d9a4c2a';
+fetch(ENDPOINT, {
+    method: 'POST', mode: 'cors', body: JSON.stringify({ params: [BNEO, "totalSupply", []], jsonrpc: "2.0", id: 1 })
+}).then(console.log)
+// cors test
