a504c8bafb01a297d0e3a370e9b9090e378d413c
vang1ong7ang <vang1ong7ang@outlook.com>
2021-10-28 02:20:49 -0400

    Update account.js

--- account.js
+++ account.js
@@ -6,10 +6,11 @@ const BASE58CHARS = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 const ADDRESS = new URLSearchParams(location.search).get('address');
 const SCRIPTHASH = `0x${[...[...ADDRESS].map(c => BASE58CHARS.indexOf(c)).reduce((acc, i) => acc.map(j => {const x = j * 58 + i; i = x >> 8; return x;}), new Uint8Array(25))].map(c => c.toString(16).padStart(2,'0')).join('').slice(8,48)}`;
 const FETCHFUNC = (ctr, method, args) => fetch(ENDPOINT, { method: 'POST', body: JSON.stringify({ params: [ctr, method, args, [{ account: BNEO, scopes: 'CalledByEntry', 'allowedcontracts': [], 'allowedgroups': [] }]], method: 'invokefunction', jsonrpc: '2.0', id: 1 }) }).then(v => v.json()).then(v => v.result);
-const DISPLAY = (id, value) => { const element = document.createElement('code'); element.innerText = value; const title = document.getElementById(id); title.parentElement.insertBefore(element, title.nextSibling); }
+const DISPLAY = (id, value) => [document.getElementById(id), document.createElement('code')].LAZY(([x, y]) => { y.innerText = value }).LAZY(([x, y]) => { x.parentElement.insertBefore(y, x.nextSibling) })
 
 new Promise(resolve => resolve(ADDRESS)).then(v => DISPLAY('account-address', v));
 FETCHFUNC(BNEO, 'balanceOf', [{type: "Hash160", value: SCRIPTHASH}]).then(v => DISPLAY('bneo-balance', v.stack[0].value));
 FETCHFUNC(BNEO, 'reward', [{type: "Hash160", value: SCRIPTHASH}]).then(v => DISPLAY('gas-unclaimed-multiplied-by-108', v.stack[0].value));
 FETCHFUNC(NEO, 'balanceOf', [{type: "Hash160", value: SCRIPTHASH}]).then(v => DISPLAY('neo-balance', v.stack[0].value));
 FETCHFUNC(GAS, 'balanceOf', [{type: "Hash160", value: SCRIPTHASH}]).then(v => DISPLAY('gas-balance-multiplied-by-108', v.stack[0].value));
+
