dc268f354467d700db687a107c1198521f1102c9
vang1ong7ang <vang1ong7ang@outlook.com>
2021-11-02 23:20:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://neofura.ngd.network:1927';
+const ENDPOINT = 'https://neofura.ngd.network';
 const NEO = '0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5';
 const GAS = '0xd2a4cff31913016155e38e474a2c06d08be276cf';
 const BNEO = '0x48c40d4666f93408be1bef038b6722404d9a4c2a';
